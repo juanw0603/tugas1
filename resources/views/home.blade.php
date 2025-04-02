d64a3974c74c11559492696e7fa4b09b6e4edb1d
juanw0603 <c14210290@john.petra.ac.id>
2025-04-02 15:17:19 +0700

    changes to card minimal height and all card are always centered

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,19 @@
 <html lang="en">
 @include('include.head')
 
-<body>
-    <div class="grid sm:grid-cols-2 xl:grid-cols-2 gap-1 px-4 py-3  ">
+<body class="flex justify-center items-center min-h-screen bg-gray-100">
+    <div class="grid  sm:grid-cols-2 xl:grid-cols-3 gap-1 px-4 py-3  ">
         @foreach ($listOfPromotion as $item)
-            <div class="flex flex-col rounded-2xl sm:w-56 bg-[#ffffff] shadow-xl">
+            <div class="flex flex-col rounded-2xl sm:w-85  bg-[#ffffff] shadow-xl">
                 <figure class="flex justify-center items-center rounded-2xl">
-                    <img src="{{ $item->image }}" alt="Card Preview" class="rounded-t-2xl">
-                </figure>
+                    <img src="{{ $item->image }}" alt="Card Preview" class="rounded-t-2xl w-full h-auto min-h-[150px] max-h-[300px] object-cover">
+                    </figure>
                 <!---->
                 <div class="flex flex-col p-8">
                     <div class="text-2xl font-bold  text-center text-[#374151] pb-6">{{$item->title}}</div>
                     <div class=" text-sm  text-center text-[#374151]">{{$item->description}}</div>
                     <div class="flex justify-end pt-6 space-x-2">
-                        <a class="w-full" href="{{ route('promotion', ['action' => 'edit']) }}">
+                        <a class="w-full" href="{{ route('promotion', ['action' => 'edit', 'id' => $item->id]) }}">
                             <button class="bg-[#000000] text-[#ffffff] w-full font-bold text-sm uppercase p-3 rounded-lg hover:bg-[#545454] active:scale-95 transition-transform transform">edit</button>
                         </a>
                         <a class="w-full" href="{{ route('promotion', ['action' => 'edit']) }}">
@@ -26,13 +26,13 @@
         @endforeach
 
 
-    <div class="flex flex-col rounded-2xl sm:w-56 bg-[#ffffff] shadow-xl">
+    <div class="flex flex-col rounded-2xl sm:w-85 bg-[#ffffff] shadow-xl">
         <figure class="flex justify-center items-center rounded-2xl">
-            <img src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" class="rounded-t-2xl">
+            <img src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" class="rounded-t-2xl w-full h-auto max-h-[300px] object-cover">
         </figure>
         <!---->
         <div class="flex flex-col p-8">
-            <div class="text-2xl font-bold  text-center text-[#374151] pb-6">Generator</div>
+            <div class="text-2xl font-bold text-center text-[#374151] pb-6">Example</div>
             <div class=" text-sm  text-center text-[#374151]">Leverage a graphical editor to create, design and customize beautiful web components.</div>
             <div class="flex justify-end pt-6 space-x-2">
                 <a class="w-full" href="{{ route('promotion', ['action' => 'edit']) }}">
